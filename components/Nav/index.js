3cc5478a112280f253fb3e4a173fd4f06aae43ef
katy-evans <katyrebeccaevans@gmail.com>
2022-04-03 21:51:50 +0100

    looking at styling the contact form, changed button colour and bg/text colours. Possibly too dark? Added spacing between nav items

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -41,7 +41,7 @@ const handleClick = () => {
           } mx-auto md:inline-flex md:flex-grow md:w-auto`}
         >
           <ul className="md:flex uppercase text-xs justify-evenly items-center md:static md:w-auto transition-all ease-in duration-500 text-gannet-700 py-2 h-screen md:h-auto text-center bg-[#222]">
-            <li className="mx-4 my-6 md:my-0">
+            <li className="mx-6 my-6 md:my-0">
               <Link href="/about">
                 <a
                   className={`hover:font-bold  ${active ? "" : "block"}`}
@@ -51,7 +51,7 @@ const handleClick = () => {
                 </a>
               </Link>
             </li>
-            <li className="mx-4 my-6 md:my-0">
+            <li className="mx-6 my-6 md:my-0">
               <Link href="/services">
                 <a
                   className={`hover:font-bold ${active ? "" : "block"}`}
@@ -61,7 +61,7 @@ const handleClick = () => {
                 </a>
               </Link>
             </li>
-            <li className="mx-4 my-6 md:my-0">
+            <li className="mx-6 my-6 md:my-0">
               <Link href="/contact">
                 <a
                   className={`hover:font-bold ${active ? "" : "block"}`}
@@ -71,7 +71,7 @@ const handleClick = () => {
                 </a>
               </Link>
             </li>
-            <li className="mx-4 my-6 md:my-0">
+            <li className="mx-6 my-6 md:my-0">
               <Link href="/">
                 <a
                   className={`hover:font-bold ${active ? "" : "hidden"}`}
