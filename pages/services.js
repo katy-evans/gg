4176116bcb4cc425bdfff814fb66c1c36b7e1d5c
katy-evans <katyrebeccaevans@gmail.com>
2022-04-13 17:17:45 +0100

    starting on the services page

--- pages/services.js
+++ pages/services.js
@@ -1,8 +1,77 @@
 import React from 'react'
+import Image from 'next/image';
 
 const Services = () => {
   return (
     <>
+		<div className="max-w-7xl mx-auto">
+			<section>
+				<div className="grid grid-cols-2 h-full">
+
+					<div className="p-2 col-span-2 flex justify-center items-center text-center pb-10">
+						<h2 className="text-gannet-500 text-4xl font-slab">Services</h2>
+					</div>
+
+					<div className="col-span-2 md:col-span-1 text-center mx-4 md:mx-0">
+						<div className="flex justify-center items-center">
+							<div className="w-full">
+								<img src="/images/image-2.jpg" alt="food" className="object-cover object-bottom h-44 w-full"/>
+							</div>
+						</div>
+					</div>
+
+					<div className="p-2 col-span-2 flex justify-center items-center text-center pb-10">
+						<h2 className="text-neutral-400 text-2xl italic">Street Food Trader</h2>
+					</div>
+
+					<div className="col-span-2 md:col-span-1 text-center mx-4 md:mx-0">
+						<div className="flex justify-center items-center">
+							<div className="w-full">
+								<img src="/images/burger.jpg" alt="food" className="object-cover"/>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-span-2 md:col-span-1 flex justify-center items-center text-center m-4 pb-6 text-[#b0c5c2]">
+						<p>
+						1 Quis elit proident laboris exercitation consequat officia do Lorem
+						quis velit commodo. Lorem labore esse adipisicing non sit cillum.
+						Irure velit ex sit Lorem et reprehenderit duis commodo velit
+						mollit.
+						</p>
+					</div>
+				</div>
+			</section>
+
+			<section className="bg-gannet-200 p-2 md:p-0">
+				<div className="grid grid-cols-2 h-full">
+				{/* <div className="grid grid-cols-2 gap-4 md:gap-8 h-full"> */}
+					<div className="col-span-2 md:col-span-1 flex justify-center items-center text-center">
+						<p className="p-8">
+						2 Quis elit proident laboris exercitation consequat officia do Lorem
+						quis velit commodo. Lorem labore esse adipisicing non sit cillum.
+						Irure velit ex sit Lorem et reprehenderit duis commodo velit
+						mollit.
+						</p>
+					</div>
+
+					<div className="col-span-2 md:col-span-1 text-center">
+						<div className="flex justify-center items-center p-2 md:p-0">
+							<div className="">
+								<img src="/images/image-2.jpg" alt="food" className="object-fit"/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<div className="col-span-2 md:col-span-1 text-center bg-[#222]">
+				<div className="flex justify-center items-center py-6 w-full p-5">
+					<button className="bg-gannet-200 p-2 border-2 border-[#222] rounded-md hover:bg-[#222] hover:text-gannet-200 hover:border-gannet-200 transition duration-300 ease-in-out">CTA contact button here?
+					</button>
+				</div>
+			</div>
+		</div>
       <div className="h-40 text-2xl flex justify-center items-center">
         <h1>Services</h1>
       </div>
