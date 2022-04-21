ac9481cd948f59af479327ba5cf022d296f5a563
katy-evans <katyrebeccaevans@gmail.com>
2022-04-21 18:45:11 +0100

    changed button color and hover color

--- pages/about.js
+++ pages/about.js
@@ -70,7 +70,7 @@ const About = () => {
 
 			<div className="col-span-2 md:col-span-1 text-center bg-[#222]">
 				<div className="flex justify-center items-center py-6 w-full p-5">
-					<button className="bg-gannet-200 p-2 border-2 border-[#222] rounded-md hover:bg-[#222] hover:text-gannet-200 hover:border-gannet-200 transition duration-300 ease-in-out">CTA contact button here?
+					<button className="bg-gannet-400 p-2 border-2 border-[#222] rounded-md hover:bg-gannet-600 hover:text-gannet-200 hover:border-gannet-200 transition duration-300 ease-in-out">Contact button here?
 					</button>
 				</div>
 			</div>
